02099fa486bf2d002ba22e6acc9232d6a1192c00
Amnariazrit <160507472+Amnariazrit@users.noreply.github.com>
2024-12-09 12:01:10 +0500

    add files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,22 @@
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Peace from "./components/Peace";
 import Products from "./components/Products";
 import SubSection from "./components/SubSection";
-import Footer from "./components/Footer";
-import LastSec from "./components/LastSec";
 
+import LastSec from "./components/LastSec";
 
 export default function Home() {
   return (
-
-
-    
     <div className="w-full h-auto bg-white">
+      <Hero />
 
+      <SubSection />
 
-<Hero />
+      <Products />
 
-<SubSection />
+      <Peace />
 
-<Products />
-
-<Peace />
-
-<LastSec />
-
-
+      <LastSec />
     </div>
   );
 }
-
-
-
